e880522851efe29af33513cdf6598d5efe8f3f33
mwoo314 <michaelwoo99@yahoo.com>
2017-02-28 22:45:06 -0500

    done

--- animate.js
+++ animate.js
@@ -1,17 +1,37 @@
 var clearButton = document.getElementById("clear");
 var svg = document.getElementById("svg");
+var lastX = -1;
+var lastY = -1;
 
 var clear = function(){
-    //clear svg
+    while(svg.hasChildNodes()){
+    	svg.removeChild(svg.lastChild);
+    }
+    lastX = -1;
+    lastY = -1;
 }
 
 var drawDot = function(){
-    newCircle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-    newCircle.setAttribute("cx", event.clientX);
-    newCircle.setAttribute("cy", event.clientY);
-    newCircle.setAttribute("radius", "25");
+	//var x = e.offsetX;
+	//var y = e.offsetY;
+    var newCircle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    var newLine = document.createElementNS("http://www.w3.org/2000/svg","line");
+    newCircle.setAttribute("cx", event.offsetX);
+    newCircle.setAttribute("cy", event.offsetY);
+    newCircle.setAttribute("r", "25");
     newCircle.setAttribute("fill", "navy");
     svg.appendChild(newCircle);
+
+    if (lastX != -1 && lastY != -1){
+    	newLine.setAttribute("x1",event.offsetX);
+    	newLine.setAttribute("y1",event.offsetY);
+    	newLine.setAttribute("x2",lastX);
+    	newLine.setAttribute("y2",lastY);
+    	newLine.setAttribute("stroke","black");
+    	svg.appendChild(newLine);
+    }
+    lastX = event.offsetX;
+    lastY = event.offsetY;
 }
 
 svg.addEventListener( "click", drawDot );
